2d817af22a3414b489e7e667093baa8e2627a7ec
Harmeet Matharoo <hmjatt@gmail.com>
2022-06-10 00:40:47 -0600

    removing dist from .gitignore

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import './style/styles.css';
-import icon from './images/cloudy.png';
-
-
-const iconAttr = document.getElementById('icon');
-iconAttr.src = icon;
-
-console.log('Webpack environment is configured for development successfully!');
