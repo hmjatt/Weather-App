f121965bb7f9e9fe2d57797892c40c1b5338ecd5
Harmeet Matharoo <hmjatt@gmail.com>
2022-06-16 14:19:00 -0600

    catched the DOM

--- src/interfaceDOM.js
+++ src/interfaceDOM.js
@@ -6,6 +6,15 @@ function interfaceDOM() {
 	const iconAttr = document.getElementById('icon');
 	iconAttr.src = icon;
 
+	//cache the DOM
+	const locationEle = document.querySelector('[data-location]');
+	const temperatureEle = document.querySelector('[data-temperature]');
+	const statusEle = document.querySelector('[data-status]');
+	const dateEle = document.querySelector('[data-date]');
+	const precipitationEle = document.querySelector('[data-precipitation]');
+	const windEle = document.querySelector('[data-wind]');
+	const humidityEle = document.querySelector('[data-humidity]');
+	const citySearchEle = document.querySelector('[data-city-search]');
 }
 
 
