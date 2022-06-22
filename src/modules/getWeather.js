46aa7e971e7141635666912b810361869883c09f
Harmeet Matharoo <hmjatt@gmail.com>
2022-06-22 14:35:25 -0600

    deleted getGif module

--- src/modules/getWeather.js
+++ src/modules/getWeather.js
@@ -1,10 +1,8 @@
 import interfaceDOM from './interfaceDOM.js';
-// import getGif from './getGif.js';
 const OPEN_WEATHER_API_KEY = process.env.OPEN_WEATHER_API_KEY;
 const GIPHY_API_KEY= process.env.GIPHY_API_KEY;
 
 document.addEventListener('DOMContentLoaded', interfaceDOM);
-// document.addEventListener('DOMContentLoaded', getGif);
 
 
 
@@ -18,15 +16,13 @@ function getWeather() {
 				const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${OPEN_WEATHER_API_KEY}`, { mode: 'cors' });
 				const getData = await response.json();
 				let description = getData.weather[0].description.split(' ').join('+');
-				// description = description;
+
 				async function gif(description) {
 					const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${GIPHY_API_KEY}&q=${description}&limit=1&offset=0&rating=g&lang=en`, { mode: 'cors' });
 					const getGifData = await response.json();
 					weatherGif.src = getGifData.data[0].images.original.url;
-					// console.log(getGifData.data[0])
 				}
 				gif(description);
-				console.log(description);
 
 			}
 			weather(city);
@@ -47,10 +43,7 @@ function getWeather() {
 		if (city === '') {
 			return;
 		} else {
-			getCurrentWeather.currentWeather(city);
-			// console.log(description)
-			// getGif();
-			// getWeatherGif.weatherGif();
+			// getCurrentWeather.currentWeather(city);
 			console.log("getting data from weather api and giphy api works");
 		}
 
--- src/public/index.js
+++ src/public/index.js
@@ -1,14 +1,11 @@
 import './style/styles.css';
 import interfaceDOM from '../modules/interfaceDOM.js';
 import getWeather from '../modules/getWeather.js';
-// import getGif from '../modules/getGif.js';
-
-// const OPEN_WEATHER_API_KEY = process.env.OPEN_WEATHER_API_KEY;
-// console.log(process.env.OPEN_WEATHER_API_KEY);
+;
 
 document.addEventListener('DOMContentLoaded', interfaceDOM);
 document.addEventListener('DOMContentLoaded', getWeather);
-// document.addEventListener('DOMContentLoaded', getGif);
+
 
 
 
