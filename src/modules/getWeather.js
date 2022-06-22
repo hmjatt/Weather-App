daf80b60af1b2fc9e503d29fb02bd6bff3eb5f07
Harmeet Matharoo <hmjatt@gmail.com>
2022-06-22 13:05:48 -0600

    can get weather data

--- src/modules/getWeather.js
+++ src/modules/getWeather.js
@@ -1,47 +1,45 @@
 import interfaceDOM from './interfaceDOM';
-// const express = require("express");
-// const axios = require('axios');
-
-// const app = express();
+// require("dotenv").config();
+const OPEN_WEATHER_API_KEY = process.env.OPEN_WEATHER_API_KEY;
 
 document.addEventListener('DOMContentLoaded', interfaceDOM);
 
 
 function getWeather() {
 
-	// // create an IIFE for appLogic
-	// const getCurrentWeather =  (() => {
-	// 	//get current weather from openweathermap API
-	// 	const currentWeather = (city) => {
-	// 		async function getWeatherNow(city) {
-	// 			// const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=apiKey`, { mode: 'cors' });
-	// 			// const getData = await response.json();
-	// 			console.log(getData)
-	// 		}
-	// 		getWeatherNow(city);
-	// 	};
+	// create an IIFE for appLogic
+	const getCurrentWeather =  (() => {
+		//get current weather from openweathermap API
+		const currentWeather = (city) => {
+			async function weather(city) {
+				const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${OPEN_WEATHER_API_KEY}`, { mode: 'cors' });
+				const getData = await response.json();
+				console.log(getData)
+			}
+			weather(city);
+		};
 		
-	// return {
-	// 	currentWeather,
-	// }
+	return {
+		currentWeather,
+	}
 
-	// })();
+	})();
 
 	citySearchForm.addEventListener('submit', (e) => {
 		const citySearchEle = document.querySelector('[data-city-search]');
 		e.preventDefault();
-		// server();
 		const city = citySearchEle.value;
 		if (city === '') {
 			return;
+		} else {
+			// getCurrentWeather.currentWeather(city);
+			console.log("getting data from weather api works");
 		}
 
-		console.log('getWeather');
-
-
-		
 	});
+
 	
 }
+
 
 export default getWeather;
--- src/public/index.js
+++ src/public/index.js
@@ -1,16 +1,13 @@
 import './style/styles.css';
 import interfaceDOM from '../modules/interfaceDOM';
 import getWeather from '../modules/getWeather';
-// require('dotenv').config();
 
-
+// const OPEN_WEATHER_API_KEY = process.env.OPEN_WEATHER_API_KEY;
+// console.log(process.env.OPEN_WEATHER_API_KEY);
 
 document.addEventListener('DOMContentLoaded', interfaceDOM);
 document.addEventListener('DOMContentLoaded', getWeather);
 
-// require("dotenv").config();
 
-// const OPEN_WEATHER_API_KEY = process.env.OPEN_WEATHER_API_KEY;
 
-// console.log(process.env.OPEN_WEATHER_API_KEY);
 
