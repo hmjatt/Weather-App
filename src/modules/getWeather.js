32de8f594de146819d39716006fc4d7e2d383e4d
Harmeet Matharoo <hmjatt@gmail.com>
2022-06-24 13:05:47 -0600

    can output city, date, temp, weather

--- src/modules/getWeather.js
+++ src/modules/getWeather.js
@@ -2,6 +2,14 @@ import interfaceDOM from './interfaceDOM.js';
 const OPEN_WEATHER_API_KEY = process.env.OPEN_WEATHER_API_KEY;
 const GIPHY_API_KEY= process.env.GIPHY_API_KEY;
 
+const locationEle = document.querySelector('[data-location]');
+const temperatureEle = document.querySelector('[data-temperature]');
+const statusEle = document.querySelector('[data-status]');
+const dateEle = document.querySelector('[data-date]');
+const precipitationEle = document.querySelector('[data-precipitation]');
+const windEle = document.querySelector('[data-wind]');
+const humidityEle = document.querySelector('[data-humidity]');
+
 
 document.addEventListener('DOMContentLoaded', interfaceDOM);
 
@@ -16,8 +24,6 @@ function getWeather() {
 
 		const currentWeather = (city) => {
 
-			// const celsius = document.getElementById('celsius');
-			// const fahrenheit = document.getElementById('fahrenheit');
 			let units;
 
 			if(celsius.checked) {
@@ -25,21 +31,36 @@ function getWeather() {
 			} else if(fahrenheit.checked) {
 				units = "&units=imperial";
 			}
-			
+	
 
 
 			async function weather(city) {
 				const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${OPEN_WEATHER_API_KEY}${units}`, { mode: 'cors' });
 				const getData = await response.json();
-				let description = getData.weather[0].description.split(' ').join('+');
+				console.log(getData, typeof(getData.main.temp));
+				locationEle.innerText = getData.name;
+				// let temps = getData.main.temp
+				temperatureEle.innerText = Math.round(getData.main.temp);
+				statusEle.innerText = getData.weather[0].description;
+				dateEle.innerText = new Date().toDateString();
 
-				async function gif(description) {
-					const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${GIPHY_API_KEY}&q=${description}&limit=1&offset=0&rating=g&lang=en`, { mode: 'cors' });
-					const getGifData = await response.json();
-					// console.log(getGifData.data[0].images);
-					weatherGif.src = getGifData.data[0].images.original.webp;
-				}
-				gif(description);
+			
+
+
+				//fill content with values
+
+
+
+				//call giphy api using weather
+				// let description = getData.weather[0].description.split(' ').join('+');
+
+				// async function gif(description) {
+				// 	const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${GIPHY_API_KEY}&q=${description}&limit=1&offset=0&rating=g&lang=en`, { mode: 'cors' });
+				// 	const getGifData = await response.json();
+				// 	// console.log(getGifData.data[0].images);
+				// 	weatherGif.src = getGifData.data[0].images.original.webp;
+				// }
+				// gif(description);
 
 			}
 			weather(city);
--- src/modules/interfaceDOM.js
+++ src/modules/interfaceDOM.js
@@ -9,13 +9,13 @@ function interfaceDOM() {
 
 
 	//cache the DOM
-	const locationEle = document.querySelector('[data-location]');
-	const temperatureEle = document.querySelector('[data-temperature]');
-	const statusEle = document.querySelector('[data-status]');
-	const dateEle = document.querySelector('[data-date]');
-	const precipitationEle = document.querySelector('[data-precipitation]');
-	const windEle = document.querySelector('[data-wind]');
-	const humidityEle = document.querySelector('[data-humidity]');
+	// const locationEle = document.querySelector('[data-location]');
+	// const temperatureEle = document.querySelector('[data-temperature]');
+	// const statusEle = document.querySelector('[data-status]');
+	// const dateEle = document.querySelector('[data-date]');
+	// const precipitationEle = document.querySelector('[data-precipitation]');
+	// const windEle = document.querySelector('[data-wind]');
+	// const humidityEle = document.querySelector('[data-humidity]');
 	const citySearchForm = document.getElementById('[citySearchForm]');
 	const weatherGif = document.getElementById('weatherGif');
 	const windImgEle = document.getElementById('wind');
